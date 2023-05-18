0926ba992461b965b0e4895959f11a6b9ba6490b
Nathan <Bowers.Nathanx@gmail.com>
2023-05-18 08:23:28 -0400

    disable submit for nonmatching passwords

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ function passVal(p1, p2, label){
   if(p1.value != p2.value & p1.value != '' & p2.value != ''){
   label.classList.add('wrong')
   p2.classList.add('wrong')
+  console.log('jeb!')
+  document.getElementById("mySubmit").disabled = true;
   }
 }
 function removeWrong(input, label){
   label.classList.remove('wrong')
   input.classList.remove('wrong')
+  document.getElementById("mySubmit").disabled = false;
+}
+function numberFormat(e){
+ var number= e.value.replace(/\D/g, '');
+ console.log('fg')
+ console.log(e.value)
+ e.value= number
+ console.log(e.value)
+ console.log(number)
 }
